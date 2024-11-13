8eb231b3426cd1c1f7dfd5c56c1fad66272a7bb0
Muhammad Fadhil Kholaf <fadhilgd123@gmail.com>
2024-11-14 00:47:04 +0700

    fix: development error

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,9 @@ const nextConfig: NextConfig = {
 };
 
 if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
+  (async () => {
+    await setupDevPlatform();
+  })();
 }
 
 export default nextConfig;
